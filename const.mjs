b8e84b8668ba1c3fe08948171f454c5d5a0ef56b
hidehidev7 <123792743+hidehidev7@users.noreply.github.com>
2026-03-14 18:05:01 +0900

    add force_server_id_channel setting

--- const.mjs
+++ const.mjs
@@ -8,5 +8,10 @@ export const settings = [
         id: "squad_allowed_channel_s",
         description: "/squadを許可するチャンネル名を半角スペース区切りで列挙",
         type: "string"
+    },
+    {
+        id: "force_server_id_channel",
+        description: "florr.ioのサーバーを指定するコードを表示するチャンネル。無指定で出力しません",
+        type: "channel"
     }
 ];
