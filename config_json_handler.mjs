97904e8e9784f6343a22ade5d2b5d5500e54489a
hidehidev7 <123792743+hidehidev7@users.noreply.github.com>
2026-04-15 20:00:52 +0900

    feat: rewrite handler with lowdb

--- config_json_handler.mjs
+++ config_json_handler.mjs
@@ -1,18 +1,4 @@
-import fs from 'fs';
-import path from 'path';
-import util from 'util';
-
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
-const getConfigJsonPath = function () {
-    return path.join(process.cwd(), "config.json");
-}
-
-const getCopyConfigJsonPath = function () {
-    return path.join(process.cwd(), "copy_config.json");
-}
-
+import { JSONFilePreset } from "lowdb/node";
 
 /**
  *  @typedef {{
@@ -23,73 +9,21 @@ const getCopyConfigJsonPath = function () {
  *  }} GuildData
  * */
 
-const configJsonDataInitializedCheck = function () {
-    if (!global.data.configJsonData) throw "data.configJsonData is not initialized";
-}
-
-const initializeAllGuildData = function (configJsonData) {
-    configJsonData.data_of_guilds.forEach(dataOfGuild => {
-        const defaultDataStructure = {
-            "configuration_permission_roles": [],
-            "settings": {}
-        };
-        assignData(dataOfGuild, defaultDataStructure);
-    })
-}
-
 /** @return { GuildData } */
 export const getDefaultGuildData = function (guildId) {
     return { "guild_id": guildId, "is_bot_activated": false, "configuration_permission_roles": [], "settings": {} };
 }
 
 export const initConfigJsonData = async function () {
-    const configJsonPath = getConfigJsonPath();
-    const copyConfigJsonPath = getCopyConfigJsonPath();
+    const configJSONDB = await JSONFilePreset('./config.json', {"data_of_guilds": [] });
+    global.data.configJsonData = configJSONDB.data;
 
-    const configJsonData = await (async () => {
-        let data;
-        try {
-            const configJsonFileReturn = await readFile(configJsonPath);
-            data = JSON.parse(configJsonFileReturn);
-        } catch (e) {
-            console.log("config.json has unexpected value. will try to read the copy file.");
-            const copyConfigJsonFileReturn = await readFile(copyConfigJsonPath);
-            data = JSON.parse(copyConfigJsonFileReturn); //note that error may happen even in here
-        }
-        return data;
-    })();
-
-    global.data.configJsonData = configJsonData;
-    console.log(`global.data.configJsonData = ${JSON.stringify(configJsonData)}`);
-
-    initializeAllGuildData(configJsonData);
-
-    //save data to config.json and copy_config.json
-
-    let lastConfigJsonFileString = "";
-    let lastCopyConfigJsonFileString = "";
-
-    setInterval(function () {
-        const configJsonFileString = JSON.stringify(global.data.configJsonData);
-        if(lastConfigJsonFileString === configJsonFileString) return;
-        lastConfigJsonFileString = configJsonFileString;
-        writeFile(configJsonPath, configJsonFileString);
-        console.log("write");
-    }, 10000);
-
-    setTimeout(() => {
-        setInterval(function () {
-            const configJsonFileString = JSON.stringify(global.data.configJsonData);
-            if(lastCopyConfigJsonFileString === configJsonFileString) return;
-            lastCopyConfigJsonFileString = configJsonFileString;
-            writeFile(copyConfigJsonPath, configJsonFileString);
-            console.log("write copy");
-        }, 10000);
-    }, 5000);
+    setInterval(async () => {
+        await configJSONDB.write();
+    }, 10_000);
 }
 
 export const getConfigJsonRef = function () {
-    configJsonDataInitializedCheck();
     return global.data.configJsonData;
 }
 
@@ -101,8 +35,6 @@ export const getGuildIndex = function (data_of_guilds, guildId) {
 //return guildData <object> | <undefined> ギルド情報を格納したオブジェクト。登録されていない場合、undefinedを返す。
 /** @return { GuildData | undefined } */
 export const getDataOfGuild = function (guildId) {
-    configJsonDataInitializedCheck();
-
     const configJsonData = global.data.configJsonData;
     if (!configJsonData) return;
 
@@ -115,11 +47,11 @@ export const getDataOfGuild = function (guildId) {
     return copyData;
 }
 
+
+
 //param guildId <String> ギルドのID
 //      guildData <Object> ギルドオブジェクト
 export const setDataOfGuild = function (guildId, guildData) {
-
-    configJsonDataInitializedCheck();
 
     const guildIndex = getGuildIndex(global.data.configJsonData.data_of_guilds, guildId);
     if (guildIndex === -1) {
@@ -191,8 +123,6 @@ const assignData = function (targetData, data) {
 }
 
 export const assignDataOfGuild = function (guildId, guildData) {
-    configJsonDataInitializedCheck();
-
     const guildIndex = getGuildIndex(global.data.configJsonData.data_of_guilds, guildId);
     if (guildIndex === -1) {
         return;
